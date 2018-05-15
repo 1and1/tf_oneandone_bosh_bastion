0a9e5d94cc80a834a592bcd63f42f308ef60aba6
alibazlamit <ali.bazlamit@hotmail.com>
2018-05-15 21:56:01 +0200

    updated create-env parameters

--- bosh.tf
+++ bosh.tf
@@ -63,7 +63,7 @@ resource "null_resource" "bosh-env" {
   provisioner "remote-exec" {
     inline = [
        "cd /opt/bosh-deployment",
-       "bosh create-env /opt/bosh-deployment/bosh.yml --state=state.json --vars-store=creds.yml -o /opt/bosh-deployment/1and1/cpi.yml -v director_name=${var.director_name} -v internal_cidr=${var.bosh_subnet} -v internal_gw=${cidrhost(var.bosh_subnet, 1)} -v internal_ip=${cidrhost(var.bosh_subnet, 2)} -v private-network-id=${oneandone_private_network.bosh_net.id} -v private_key=/root/.ssh/id_rsa -v api_token=${var.api_token} -v ssh_key=\"$(cat /root/.ssh/id_rsa.pub)\""
+       "bosh create-env /opt/bosh-deployment/bosh.yml --state=state.json --vars-store=creds.yml -o /opt/bosh-deployment/1and1/cpi.yml -v director_name=${var.director_name} -v internal_cidr=${var.bosh_subnet} -v internal_gw=${cidrhost(var.bosh_subnet, 1)} -v internal_ip=${cidrhost(var.bosh_subnet, 2)} -v private_network_id=${oneandone_private_network.bosh_net.id} -v datacenter=${var.datacenter} -v private_key=/root/.ssh/id_rsa -v api_token=${var.api_token} -v ssh_key=\"$(cat /root/.ssh/id_rsa.pub)\""
      ]
   }
 }
